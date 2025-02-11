de37d8ad9b737fe1e51b4367d98cf6bb8aedccbe
Anastasiia Savchyn <sa@empat.tech>
2025-02-11 20:35:48 +0200

    task 1 lab 3

--- lib/initial/screens/task_list_screen.dart
+++ lib/initial/screens/task_list_screen.dart
@@ -5,6 +5,7 @@ import 'package:flutter_labs/initial/screens/task_details_screen.dart';
 import 'package:flutter_labs/labs/lab1/task1/screens/fuel_calculator_screen.dart';
 import 'package:flutter_labs/labs/lab1/task2/screens/fuel_oil_calculator_screen.dart';
 import 'package:flutter_labs/labs/lab2/task1/screens/emission_calculator_screen.dart';
+import 'package:flutter_labs/labs/lab3/screens/solar_calculator_screen.dart';
 
 class TaskListScreen extends StatelessWidget {
   final Lab lab;
@@ -21,7 +22,7 @@ class TaskListScreen extends StatelessWidget {
       case 2:
         return [const Task("1", "Emission Calculator", EmissionCalculatorScreen())];
       case 3:
-        return [Task("1", "Task 1", Container())];
+        return [Task("1", "Solar Calculator", SolarCalculatorScreen())];
       case 4:
         return [Task("1", "Task 1", Container())];
       case 5:
